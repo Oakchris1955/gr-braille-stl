6602eba41032d9fb9cb48a7afb2e150aa3e1e695
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2025-04-03 19:48:50 +0300

    fix: the resulting STL now conforms to Braille sizing and spacing standards

--- src/cad.py
+++ src/cad.py
@@ -2,69 +2,47 @@ from typing import List
 import build123d as bd
 
 from braille_symbol import BrailleSymbol
+import braille_constants as CONSTS
 
-class CADParams:
-    dot_radius: float
-    cell_width: float
-    cell_height: float
-    dot_height: float
-
-    def __init__(self, dot_radius: float, cell_width: float, cell_height: float, dot_height: float):
-        self.dot_radius = dot_radius
-        self.cell_width = cell_width
-        self.cell_height = cell_height
-        self.dot_height = dot_height
-
-    @classmethod
-    def from_radius_and_height(cls, radius: float, height: float):
-        return cls(radius, radius * 4, height / 2, height / 2)
-
-    def symbol_width(self) -> float:
-        return self.cell_width * 2
-
-    def symbol_length(self) -> float:
-        return self.cell_width * 3
-
-    def symbol_height(self) -> float:
-        return self.dot_height + self.cell_height
-
-
-def _generate_dots(symbols: List[BrailleSymbol], params: CADParams) -> bd.Part:
-    dot_cylinder = bd.Cylinder(params.dot_radius, params.symbol_height(), align=bd.Align.MIN)
+def _generate_dots(symbols: List[BrailleSymbol]) -> bd.Part:
+    dot_cylinder = bd.Cylinder(CONSTS.DOT_DIAMETER / 2, CONSTS.DOT_HEIGHT * 2, align=bd.Align.MIN)
 
     dots = bd.Part()
 
-    symbol_pos = bd.Vector(params.cell_width / 2, -params.cell_width / 2 + params.cell_width * 3, 0)
+    symbol_pos = bd.Vector(CONSTS.DOT_DISTANCE / 2, -CONSTS.DOT_DISTANCE / 2 + CONSTS.DOT_DISTANCE * 3, 0)
 
     for symbol in symbols:
         for i, dot in enumerate(symbol.dots):
             if dot:
                 dots += bd.copy_module.copy(dot_cylinder).locate(bd.Pos(symbol_pos))
 
-            symbol_pos.Y -= params.cell_width
+            symbol_pos.Y -= CONSTS.DOT_DISTANCE
 
             if (i+1) % 3 == 0:
-                symbol_pos.Y += params.cell_width * 3
-                symbol_pos.X += params.cell_width
+                symbol_pos.Y += CONSTS.DOT_DISTANCE * 3
+                symbol_pos.X += CONSTS.DOT_DISTANCE
+
+        symbol_pos.X -= CONSTS.DOT_DISTANCE * 2
+        symbol_pos.X += CONSTS.CELL_HORIZONTAL_DISTANCE
 
     return dots
 
-def _generate_base(params: CADParams, width: int, rows: int) -> bd.Part:
+def _generate_base(width: int, rows: int) -> bd.Part:
     # the coordinates we give are the center of the box, but we want one edge to be at the axis origin
-    return bd.Box(width * params.symbol_width(), rows * params.symbol_length(), params.cell_height, align=bd.Align.MIN)
+    return bd.Box(width * CONSTS.CELL_HORIZONTAL_DISTANCE, rows * CONSTS.CELL_VERTICAL_DISTANCE, CONSTS.DOT_HEIGHT, align=bd.Align.MIN)
 
-def braille_to_stl(symbols: List[List[BrailleSymbol]], params: CADParams, dest_file: str):
+def braille_to_stl(symbols: List[List[BrailleSymbol]], dest_file: str):
     print("Generating mesh...")
 
     rows = len(symbols)
     width = max(len(s) for s in symbols)
 
-    base = _generate_base(params, width, rows)
+    base = _generate_base(width, rows)
     dots = bd.Part()
 
     for i, s in enumerate(symbols):
-        current_row = _generate_dots(s, params)
-        current_row.position += (0, (rows - i - 1) * params.symbol_length(), 0)
+        current_row = _generate_dots(s)
+        current_row.position += (0, (rows - i - 1) * CONSTS.CELL_VERTICAL_DISTANCE, 0)
         dots += current_row
 
     print("Finalizing mesh (this may take a while)...")
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 import argparse
 
 from braille_converter import text_to_braille
-from cad import braille_to_stl, CADParams
+from cad import braille_to_stl
 
 DEFAULT_RADIUS: float = 0.1
 DEFAULT_HEIGHT: float = 0.2
@@ -15,13 +15,11 @@ def main():
     )
     parser.add_argument('text', help="Το κείμενο που θέλετε να μεταφραστεί")
     parser.add_argument('dest', help="Η τοποθεσία του αρχείου εξόδου")
-    parser.add_argument('-r', '--radius', help="Η ακτίνα των κουκίδων", type=float, default=DEFAULT_RADIUS)
-    parser.add_argument('--height', help="Το συνολικό ύψος του STL μοντέλου", type=float, default=DEFAULT_HEIGHT)
 
     args = parser.parse_args()
 
     s = [text_to_braille(t) for t in args.text.split(("\n"))]
-    braille_to_stl(s, CADParams.from_radius_and_height(args.radius, args.height), args.dest)
+    braille_to_stl(s, args.dest)
 
 if __name__ == "__main__":
     main()
